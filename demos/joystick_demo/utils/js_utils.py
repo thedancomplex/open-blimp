6ba7192d454990c1f582c0f328f12b1980c15711
Tony Lin <txlin8@gmail.com>
2022-07-05 14:47:22 -0400

    bug fixes, speed improvements

--- demos/joystick_demo/utils/js_utils.py
+++ demos/joystick_demo/utils/js_utils.py
@@ -6,8 +6,6 @@ class JoyStick_helper:
         pygame.joystick.init()
         self.js = pygame.joystick.Joystick(js_id)
         
-        self.button_state = False
-        self.toggle_state = False
         self.dead = 0.2
 
     def get_state(self):
@@ -22,14 +20,7 @@ class JoyStick_helper:
         rlr = rlr*(not (abs(rlr) < self.dead))
         rud = rud*(not (abs(rud) < self.dead))
         
-        # get debounced button value
-        button_event = False
-        if self.js.get_button(3) and not self.button_state: 
-            self.button_state = True
-            self.toggle_state = not self.toggle_state
-            button_event = True
-          
-        elif not self.js.get_button(3) and self.button_state:
-            self.button_state = False
+        lshoulder = self.js.get_button(4)
+        rshoulder = self.js.get_button(5)
 
-        return [llr, lud, rlr, rud], button_event, self.toggle_state
+        return [llr, lud, rlr, rud], lshoulder, rshoulder
