8d81eb778aa889600b263d235621b5272d0593f2
Tony Lin <txlin8@gmail.com>
2022-07-06 10:12:55 -0400

    stream tuning

--- demos/joystick_demo/js_demo.py
+++ demos/joystick_demo/js_demo.py
@@ -19,8 +19,8 @@ b2 = Blimp(2, port, ser, pi2_ports)
 js = JoyStick_helper()
 
 # desired altitude holding
-des_z1 = 1.5
-des_z2 = 1.5
+des_z1 = 0.5
+des_z2 = 0.5
 
 # heading rate and heading angle
 kg = 0.05 
@@ -30,76 +30,88 @@ des_yw2 = 0.
 # show the active image
 fig, axes = plt.subplots(1,1)
 
+# state flags
+b1_first = False
+b2_first = False
+
 # main loop
 while True:
     # handle the joystick
     ax, b1_active, b2_active = js.get_state()
      
     if b1_active:
-        if b1.pi.bno_new:
-            # update states from the pi
-            b1.poll_bno()
-
-            # commit velocity control  
-            des_vx = -ax[1]
-            des_vy = -ax[0]
-            des_yw1 += kg*ax[2]
-            des_yw1 += 2*np.pi*(des_yw1 < -np.pi) - 2*np.pi*(des_yw1 > np.pi)
-
-            b1.set_vel([des_vy, des_vx])
-            b1.set_heading(des_yw1)
-
-        if b1.pi.dist_new:
-            # update states from the pi
-            b1.poll_dist()
-
-            # commit altitude control
-            des_z1 += kg*ax[3]
-            des_z1 = min(max(des_z1, 0.5), 2.0)
-            b1.set_alt(des_z1, positive_only=True)
-        
-        if b1.pi.cam_new:
-            b1.poll_image()
-            axes.clear()
-            axes.imshow(b1.I)
-            axes.set_title("Blimp 1 View")
-            plt.draw()
+        if not b1_first:
+            b1_first = True
+            b1.zero_z_rot()
             
-    elif b2_active:    
-        if b2.pi.bno_new:
-            # update states from the pi
-            b2.poll_bno()
+        # update states from the pi
+        b1.poll_bno()
 
-            # commit velocity control  
-            des_vx = -ax[1]
-            des_vy = -ax[0]
-            des_yw2 += kg*ax[2]
-            des_yw2 += 2*np.pi*(des_yw2 < -np.pi) - 2*np.pi*(des_yw2 > np.pi)
+        # commit velocity control  
+        des_vx = -ax[1]
+        des_vy = -ax[0]
+        des_yw1 += kg*ax[2]
+        des_yw1 += 2*np.pi*(des_yw1 < -np.pi) - 2*np.pi*(des_yw1 > np.pi)
 
-            b2.set_vel([des_vy, des_vx])
-            b2.set_heading(des_yw2)
+        b1.set_vel([des_vy, des_vx])
+        b1.set_heading(des_yw1)
 
-        if b2.pi.dist_new:
-            # update states from the pi
-            b2.poll_dist()
+        # update states from the pi
+        b1.poll_dis()
 
-            # commit altitude control
-            des_z2 += kg*ax[3]
-            des_z2 = min(max(des_z2, 0.5), 2.0)
-            b2.set_alt(des_z2, positive_only=True)
-
-        if b2.pi.cam_new:
-            b2.poll_image()
-            axes.clear()
-            axes.imshow(b2.I)
-            axes.set_title("Blimp 2 View")
-            plt.draw()
-            
-    else:
+        # commit altitude control
+        des_z1 += kg*ax[3]
+        des_z1 = min(max(des_z1, 0.5), 2.0)
         b1.set_alt(des_z1, positive_only=True)
+        
+        b1.poll_image()
+        axes.clear()
+        axes.imshow(b1.I)
+        axes.set_title("Blimp 1 View")
+        plt.draw()
+
+        b1.step()
+
+            
+    elif b2_active:   
+        if not b2_first:
+            b2_first = True
+            b2.zero_z_rot()
+
+        # update states from the pi
+        b2.poll_bno()
+        [r, p, yw] = b2.euler()
+        
+        # commit velocity control  
+        des_vx = -ax[1]
+        des_vy = -ax[0]
+        des_yw2 += kg*ax[2]
+        des_yw2 += 2*np.pi*(des_yw2 < -np.pi) - 2*np.pi*(des_yw2 > np.pi)
+
+        b2.set_vel([des_vy, des_vx])
+        b2.set_heading(des_yw2)
+
+        # update states from the pi
+        b2.poll_dis()
+
+        # commit altitude control
+        des_z2 += kg*ax[3]
+        des_z2 = min(max(des_z2, 0.5), 2.0)
         b2.set_alt(des_z2, positive_only=True)
 
-    # send the command
-    b1.step()
-    b2.step()
-    plt.pause(0.001)
+        b2.poll_image()
+        axes.clear()
+        axes.imshow(b2.I)
+        axes.set_title("Blimp 2 View")
+        plt.draw()
+
+        b2.step()
+
+    else:        
+        if b1_first:
+            b1_first = False
+        
+        if b2_first:
+            b2_first = False
+        
+    plt.pause(0.01)
