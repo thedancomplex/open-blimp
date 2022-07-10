6cf49b97ddb808a1d7d83868ba773f49d8574f26
tony-x-lin <txlin8@gmail.com>
2022-07-10 19:46:03 -0400

    merge

--- src/piStreaming/stream_pi02.py
+++ src/piStreaming/stream_pi02.py
@@ -215,7 +215,8 @@ class MultiStream:
         # setup the bno055
         sensor = BNO055.BNO055(serial_port='/dev/serial0', rst=18)
         sensor.set_mode(8) # 8- IMU mode, 12 - NDOF mode
-        
+        bno.begin()
+
         # setup shared memory flag
         sh_flag = sm.SharedMemory(fname)
         flag = np.ndarray(1, dtype=np.bool_, buffer=sh_flag.buf)
